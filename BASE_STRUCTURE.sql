87c842a5788c98704600fe46a874b2993715b97d
Baptiste Cadiou <bc@ViZioCode.com>
2019-09-23 20:01:59 +0200

    Add files via upload

---
+++ BASE_STRUCTURE.sql
@@ -0,0 +1,499 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  lun. 23 sep. 2019 à 17:29
+-- Version du serveur :  10.1.38-MariaDB
+-- Version de PHP :  7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `timegraph`
+--
+CREATE DATABASE IF NOT EXISTS `timegraph` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `timegraph`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `class`
+--
+
+CREATE TABLE `class` (
+  `id` int(11) NOT NULL,
+  `name` varchar(32) CHARACTER SET latin1 NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci ROW_FORMAT=DYNAMIC;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `concept`
+--
+
+CREATE TABLE `concept` (
+  `id` int(11) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `station_id` int(11) NOT NULL DEFAULT '1',
+  `code` varchar(5) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `demande`
+--
+
+CREATE TABLE `demande` (
+  `datetime` datetime NOT NULL,
+  `titre` text NOT NULL,
+  `delivery` datetime DEFAULT NULL,
+  `created_by` varchar(64) NOT NULL,
+  `modified_by` varchar(64) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `format`
+--
+
+CREATE TABLE `format` (
+  `id` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `group`
+--
+
+CREATE TABLE `group` (
+  `id` int(11) NOT NULL,
+  `name` varchar(40) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `livraison`
+--
+
+CREATE TABLE `livraison` (
+  `datetime` datetime NOT NULL,
+  `titre` text NOT NULL,
+  `image` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `log_mos`
+--
+
+CREATE TABLE `log_mos` (
+  `datetime` datetime NOT NULL,
+  `outtime` datetime NOT NULL,
+  `id` int(11) NOT NULL,
+  `concept` varchar(32) NOT NULL,
+  `template_id` int(11) NOT NULL,
+  `titre` varchar(64) NOT NULL,
+  `query` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `memory`
+--
+
+CREATE TABLE `memory` (
+  `ticket_id` int(11) NOT NULL,
+  `pile_num_row` int(11) NOT NULL,
+  `last_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pile_mos`
+--
+
+CREATE TABLE `pile_mos` (
+  `datetime` datetime NOT NULL,
+  `id` int(11) NOT NULL,
+  `template_id` int(11) NOT NULL,
+  `concept` varchar(32) NOT NULL,
+  `titre` varchar(64) NOT NULL,
+  `query` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `slug`
+--
+
+CREATE TABLE `slug` (
+  `thread` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `group_id` int(11) NOT NULL DEFAULT '1',
+  `station_id` int(11) NOT NULL DEFAULT '1',
+  `concept_id` int(11) NOT NULL DEFAULT '0',
+  `class_id` int(11) NOT NULL DEFAULT '0',
+  `system_id` int(11) NOT NULL DEFAULT '0',
+  `format_id` int(11) NOT NULL DEFAULT '0',
+  `client` varchar(128) NOT NULL,
+  `path` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `snapshot`
+--
+
+CREATE TABLE `snapshot` (
+  `id` int(11) NOT NULL,
+  `template_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `size` varchar(25) NOT NULL,
+  `type` varchar(25) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `payload` mediumblob NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `station`
+--
+
+CREATE TABLE `station` (
+  `id` int(11) NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `group_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `system`
+--
+
+CREATE TABLE `system` (
+  `id` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tag`
+--
+
+CREATE TABLE `tag` (
+  `id` int(11) NOT NULL,
+  `name` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `template`
+--
+
+CREATE TABLE `template` (
+  `concept_id` int(11) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `variant` varchar(40) COLLATE latin1_general_ci NOT NULL,
+  `version` int(11) NOT NULL,
+  `system_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL,
+  `file` text CHARACTER SET latin1 NOT NULL,
+  `description` text CHARACTER SET latin1 NOT NULL,
+  `author` varchar(80) CHARACTER SET latin1 NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `rating` double NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `format` varchar(20) COLLATE latin1_general_ci NOT NULL,
+  `size` varchar(20) COLLATE latin1_general_ci NOT NULL,
+  `category` text COLLATE latin1_general_ci NOT NULL,
+  `popularity` int(1) NOT NULL,
+  `comment` text COLLATE latin1_general_ci NOT NULL,
+  `background` varchar(64) COLLATE latin1_general_ci NOT NULL,
+  `layer` text COLLATE latin1_general_ci NOT NULL,
+  `state` text COLLATE latin1_general_ci NOT NULL,
+  `control` text COLLATE latin1_general_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci ROW_FORMAT=DYNAMIC;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `id` int(11) NOT NULL,
+  `thread` int(11) NOT NULL,
+  `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ip` varchar(15) NOT NULL,
+  `level` int(1) NOT NULL,
+  `initials` varchar(3) NOT NULL,
+  `body` text NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `type` varchar(25) NOT NULL,
+  `snapshot` mediumblob NOT NULL,
+  `uid` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Ticketing';
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `time`
+--
+
+CREATE TABLE `time` (
+  `group_id` int(11) NOT NULL,
+  `station_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `concept_id` int(11) NOT NULL,
+  `thread` int(11) NOT NULL,
+  `start` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `stop` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `username` varchar(32) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `id` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `watchdog`
+--
+
+CREATE TABLE `watchdog` (
+  `template_id` int(11) NOT NULL,
+  `field_name` varchar(32) NOT NULL,
+  `bark_value` varchar(64) NOT NULL,
+  `template_name` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `class`
+--
+ALTER TABLE `class`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `concept`
+--
+ALTER TABLE `concept`
+  ADD PRIMARY KEY (`id`,`station_id`);
+
+--
+-- Index pour la table `demande`
+--
+ALTER TABLE `demande`
+  ADD PRIMARY KEY (`datetime`),
+  ADD KEY `datetime` (`datetime`);
+
+--
+-- Index pour la table `format`
+--
+ALTER TABLE `format`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `group`
+--
+ALTER TABLE `group`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `livraison`
+--
+ALTER TABLE `livraison`
+  ADD PRIMARY KEY (`datetime`),
+  ADD KEY `datetime` (`datetime`);
+
+--
+-- Index pour la table `log_mos`
+--
+ALTER TABLE `log_mos`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Index pour la table `pile_mos`
+--
+ALTER TABLE `pile_mos`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Index pour la table `slug`
+--
+ALTER TABLE `slug`
+  ADD UNIQUE KEY `thread` (`thread`);
+
+--
+-- Index pour la table `snapshot`
+--
+ALTER TABLE `snapshot`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `station`
+--
+ALTER TABLE `station`
+  ADD PRIMARY KEY (`id`,`group_id`);
+
+--
+-- Index pour la table `system`
+--
+ALTER TABLE `system`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `tag`
+--
+ALTER TABLE `tag`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Index pour la table `template`
+--
+ALTER TABLE `template`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `time`
+--
+ALTER TABLE `time`
+  ADD PRIMARY KEY (`id`,`group_id`,`station_id`) USING BTREE;
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Index pour la table `watchdog`
+--
+ALTER TABLE `watchdog`
+  ADD UNIQUE KEY `template_id` (`template_id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `class`
+--
+ALTER TABLE `class`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `concept`
+--
+ALTER TABLE `concept`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `format`
+--
+ALTER TABLE `format`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `group`
+--
+ALTER TABLE `group`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `snapshot`
+--
+ALTER TABLE `snapshot`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `station`
+--
+ALTER TABLE `station`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `system`
+--
+ALTER TABLE `system`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `tag`
+--
+ALTER TABLE `tag`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `template`
+--
+ALTER TABLE `template`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `time`
+--
+ALTER TABLE `time`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
