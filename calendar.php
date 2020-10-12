e7dbd90b472727e3fde0fd6b8081c948af5c77b6
Baptiste Cadiou <cadiou@me.com>
2020-10-12 12:25:30 +0200

    optimisation calendar

--- calendar.php
+++ calendar.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * 200928
+ * 201011
  * timeticket / calendar
  * bcadiou@videlio-globalservices.com
  *
@@ -20,20 +20,13 @@ $html = new HTML(substr($date1,0,10)." &rarr; ".substr($date2,0,10),30);
 
 $html->module_calendar();
 
-############################################################## JOURS
-
 $table = "<table>";
-
 $table.= '<tr><td colspan=4 class=slug><a href="calendar.php?n='.($n+1).'&year='.date("Y",strtotime($year."W".$week."7-".($n+1)."week")).'&week='.
 		date("W",strtotime($year."W".$week."7-".($n+1)."week")).
 		'">&larr;</a></td><td colspan=4 class=slug_droite><a href="calendar.php?n='.($n+1).'&year='.date("Y",strtotime($year."W".$week."7+".($n+1)."week")).'&week='.
                 date("W",strtotime($year."W".$week."7+".($n+1)."week")).
                 '">&rarr;</a></td></tr>';
-
-#$table.= "<tr><td>SEMAINE</td><td>LUNDI</td><td>MARDI</td><td>MERCREDI</td><td>JEUDI</td><td>VENDREDI</td><td>SAMEDI</td><td>DIMANCHE</td></tr>";
-
 $table.="<tr><td>Week</td>";
-
 for ($i = 0; $i <7 ; $i++) {
 	$table.= "<td>";
 	if ((date("w")==($i+1)) or (date("w")==0 and $i==6)) {
@@ -45,15 +38,10 @@ for ($i = 0; $i <7 ; $i++) {
         }
 	$table.= "</td>";
 }
-
 $table.="</tr>";
-
 for ($i = 0; $i <= $n; $i++) {
-
 	# CLASSES DE LA SEMAINE
-
 	$unixdate=strtotime($year."W".$week."+".$i."week");
-
 	$query = "SELECT class_id".
                                  " FROM event WHERE station_id = ".CONFIG::ID_STATION.
                                  " AND not(unix_timestamp(start)-(86400*7) > ".$unixdate.
@@ -61,39 +49,22 @@ for ($i = 0; $i <= $n; $i++) {
                                  " AND not(unix_timestamp(start) < ".$unixdate.
                                  " AND unix_timestamp(stop) < ".$unixdate.")".
                                  " GROUP by class_id ORDER by class_id";
-
         $result_class =  $html->query($query);
-
-
 	# BARRE DE DATES DE LA SEMAINE
-
 	$table.="<tr>";
-
 	$table.="<td><h2>".date("W",strtotime($year."W".$week."7+".$i."week"))."</h2></td>";
-
 	for ($j = 0;$j < 7;$j++) {
-
 		$table.="<td>";
-
 		$unixdate=strtotime($year."W".$week."+".(($i*7)+$j)."day");
-
 		$table.="<h2>".date( "d M", $unixdate)."</h2>";
-
 		$table.="</td>";
 	}
-
 	$table.="</tr>";
-
 	# UNE LIGNE PAR CLASSE
-
 	if (mysqli_num_rows($result_class)!=0) {
-
 		while ($item_class = mysqli_fetch_array($result_class)) {
-
 			$table.="<tr>";
-
 			# AFFICAGE DE LA CLASSE
-
                         if ($item_class[0]>0) {
                         	$query = "SELECT class.name ".
                                 	" FROM `class`".
@@ -106,20 +77,11 @@ for ($i = 0; $i <= $n; $i++) {
                         }else{
                            	$class = "Lazarus";
                         }
-
 			$table.= "<td>".$class."</td>";
-
-
 			# JOURS PAR CLASSE
-
         		for ($j = 0;$j < 7;$j++) {
-
 				$unixdate=strtotime($year."W".$week."+".(($i*7)+$j)."day");
-
               			$table.="<td>";
-
-
-
 				$query = "SELECT id,name,class_id,format_id,start,stop,client,vendeur,reference,level".
 				 " FROM event WHERE station_id = ".CONFIG::ID_STATION." and class_id = ".$item_class[0].
 				 " AND not(unix_timestamp(start)-86400 > ".$unixdate.
@@ -127,12 +89,8 @@ for ($i = 0; $i <= $n; $i++) {
 				 " AND not(unix_timestamp(start) < ".$unixdate.
                                  " AND unix_timestamp(stop) < ".$unixdate.")".
 				 " ORDER by class_id DESC,start,level";
-
 				$result =  $html->query($query);
-
-
 				if (mysqli_num_rows($result)!=0) {
-
         				while ($item = mysqli_fetch_array($result)) {
 						if (date( "d M",strtotime($item[4]))==date( "d M",$unixdate)) {
 							$time = date( "H:i",strtotime($item[4]));
@@ -149,31 +107,19 @@ for ($i = 0; $i <= $n; $i++) {
                                 		}
 
 						$time.="<br>";
-
 						$table.= "<a href=\"calendar.php?id=".$item[0]."\"><h4 class=\"level".$item[9]."\">";
 						$table.= $time;
 						$table.= stripslashes(($item[1]!=""?$item[1]:"Lazarus"));
 						$table.= "</h4></a>";
        				 	}
-
 				}
-
 				$table.="</td>";
-
 			}
-
-
-
 			$table.="</tr>";
-
 		}
-
 	}
-
 	$table.="<tr><td colspan=8><hr></td></tr>";
-
 }
-
 $table.= "</table>";
 
 $html->body($table);
